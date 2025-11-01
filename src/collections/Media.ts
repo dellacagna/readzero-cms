de4d9cf3eb8fa1930e6cb002f88aa5b3bd5da2c7
Giovanni Della Cagna <dellacagna@gmail.com>
2025-11-01 22:17:56 +0100

    Docker Stuff

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,9 +1,23 @@
 import type { CollectionConfig } from 'payload'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
+  },
+  upload: {
+    staticDir: path.resolve(dirname, '../media'),
+
+    imageSizes: [
+      { name: 'thumbnail', width: 320, height: 320, fit: 'cover' },
+      { name: 'medium', width: 1024 },
+    ],
+    adminThumbnail: 'thumbnail',
   },
   fields: [
     {
@@ -11,6 +25,5 @@ export const Media: CollectionConfig = {
       type: 'text',
       required: true,
     },
-  ],
-  upload: true,
+  ]
 }
